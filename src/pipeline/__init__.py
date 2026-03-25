1080120974b5fd8d6afc96a828448e21dac50762
Letera Tujo <letera9tujo@gmail.com>
2026-03-25 23:09:37 +0300

    complet

---
+++ src/pipeline/__init__.py
@@ -0,0 +1,6 @@
+"""ML pipeline module."""
+
+from .training_pipeline import TrainingPipeline
+from .inference_pipeline import InferencePipeline
+
+__all__ = ['TrainingPipeline', 'InferencePipeline']
